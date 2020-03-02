b253cc2f5cc98dc8a7d11ac244a18db4196899c4
johnflentri <jpflentri@gmail.com>
2020-03-02 11:49:49 +0100

    Image table created

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const app = express()
-const db = require("./db")
 
+const db = require("./db")
+const Image = require("./image/model")
 const port = process.env.PORT || 4000
 
 app.listen(port, () => console.log("Listening on port:", port))
